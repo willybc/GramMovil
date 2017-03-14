8922a565c422cf2ec87f564b31badb0b750473e4
Willy Bardales <willybc06@gmail.com>
2017-03-14 20:38:38 -0300

    Fix Intens

--- src/main/java/io/github/httpswillybc/appmovil/CreateAccountActivity.java
+++ src/main/java/io/github/httpswillybc/appmovil/CreateAccountActivity.java
@@ -7,30 +7,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import io.github.httpswillybc.appmovil.R;
+
 public class CreateAccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        showToolbar(getResources().getString(R.string.toolbar_tittle_createaccount), false);
+        showToolbar(getResources().getString(R.string.toolbar_tittle_createaccount), true);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     public void showToolbar(String tittle, boolean upButton){
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setTitle(tittle);
         getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 
--- src/main/java/io/github/httpswillybc/appmovil/MainActivity.java
+++ src/main/java/io/github/httpswillybc/appmovil/MainActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
 
-import io.github.httpswillybc.appmovil.CreateAccountActivity;
-
 public class MainActivity extends AppCompatActivity {
 
 
