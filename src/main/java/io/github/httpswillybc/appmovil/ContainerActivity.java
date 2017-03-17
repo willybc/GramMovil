7448ca36055b21f19b1d79f57bb1207a5b42e704
Willy Bardales <willybc06@gmail.com>
2017-03-17 19:45:36 -0300

    Fragments en nuestro BottomBar

--- src/main/java/io/github/httpswillybc/appmovil/ContainerActivity.java
+++ src/main/java/io/github/httpswillybc/appmovil/ContainerActivity.java
@@ -2,6 +2,20 @@ package io.github.httpswillybc.appmovil;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.annotation.IdRes;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.github.httpswillybc.appmovil.R;
+import io.github.httpswillybc.appmovil.view.fragment.HomeFragment;
+import io.github.httpswillybc.appmovil.view.fragment.ProfileFragment;
+import io.github.httpswillybc.appmovil.view.fragment.SearchFragment;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabReselectListener;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class ContainerActivity extends AppCompatActivity {
 
@@ -9,5 +23,35 @@ public class ContainerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
+
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottombar);
+        bottomBar.setDefaultTab(R.id.home);
+
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                switch (tabId){
+                    case R.id.home:
+                        HomeFragment homeFragment = new HomeFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null).commit();
+
+                        break;
+                    case R.id.Profile:
+                        ProfileFragment ProfileFragment = new ProfileFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, ProfileFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null).commit();
+                        break;
+                    case R.id.search:
+                        SearchFragment SearchFragment = new SearchFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, SearchFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null).commit();
+                        break;
+                }
+            }
+        });
     }
 }
